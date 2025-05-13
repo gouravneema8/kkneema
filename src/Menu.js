04d3a9eeb452cb89bd410ca071ce74fc8f4d4f06
gourav neema <gourav.neema8@gmail.com>
2025-05-13 19:27:08 +0530

    some changes has done

--- src/Menu.js
+++ src/Menu.js
@@ -8,12 +8,14 @@ import Home from "./Home";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+
+
 function Menu()
 {
     return(
         <>
 <BrowserRouter>
-<div className="container-fluid">
+<div className="menu1">
 <Navbar bg="warning" data-bs-theme="dark">
         <Container>
           <Navbar.Brand style={{color:"black"}}>KKNeema</Navbar.Brand>
@@ -27,6 +29,31 @@ function Menu()
         </Container>
       </Navbar>
 </div>
+
+{/* for responsive design small size from line 11*/}
+<div className="container-fluid menu2">
+<Navbar collapseOnSelect expand="lg" bg="warning">
+      <Container>
+        <Navbar.Brand href="#home">KKNeema</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Navbar.Brand><NavLink to='/' style={{textDecoration:"none",color:"black"}}>Home</NavLink></Navbar.Brand>
+          <Navbar.Brand><NavLink to='/Bhajhan' style={{textDecoration:"none",color:"black"}}>Bhajhan</NavLink></Navbar.Brand>
+          <Navbar.Brand><NavLink to='/Poem' style={{textDecoration:"none",color:"black"}}>Poem</NavLink></Navbar.Brand>
+          <Navbar.Brand><NavLink to='/Aboutus' style={{textDecoration:"none",color:"black"}}>About</NavLink></Navbar.Brand>          
+          </Nav>
+           </Navbar.Collapse>
+      </Container>
+    </Navbar>
+</div>
+
+
+
+
+{/* for responsive design small size */}
+
+
 <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/Bhajhan" element={<Bhajhan/>}/>
